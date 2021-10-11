060b20fa6b8c5050e9842d760deeccd77269d054
Gustavo Martins <gm357191@gmail.com>
2021-10-11 18:20:14 -0300

    creating lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/item';
 
 const App = () => {
+
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'comprar pão', done: false}
+  ]);
+
+
   return (
     <C.Container>
       <C.Area>
