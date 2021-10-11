456b8faa443f8a580f529914460bd8e23f52a0e4
Gustavo Martins <gm357191@gmail.com>
2021-10-11 18:05:50 -0300

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import * as C from './App.styles';
 const App = () => {
   return (
     <C.Container>
-      
+      <C.Area>
+        <C.Header>Lista de tarefas</C.Header>
+      </C.Area>
     </C.Container>
   );
 }
