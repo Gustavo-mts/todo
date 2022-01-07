1c01436a2e8fc8e19963b7a5c926e5bd313dd120
Gustavo Martins <gm357191@gmail.com>
2022-01-07 01:31:41 -0300

    server.ts

--- server.ts
+++ server.ts
@@ -8,13 +8,14 @@ app.use('/',
     express.static(
         resolve(
             __dirname,
-            './buid'
+            'build',
+            'index.html'
         )
     )
 )
 
 app
-    .listen(process.env.PORT || 3000, (err) => {
+    .listen(process.env.PORT || 3000, (err: any) => {
         if(err) { return console.log(err) }
 
         console.log("tudo funcionando...");
